9ca8dad7b23878813a10c8030c5f9c0d9fda3eed
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-09-28 12:53:35 -0700

    init

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+### SCHEMA
+drop if exists burgers_db;
+CREATE DATABASE burgers_db;
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	burger_name varchar(255) NOT NULL,
+	devoured boolean NOT NULL,
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO clients (burgers) VALUES 
+('4x4',false),('3x3',true),('2x2',false);
