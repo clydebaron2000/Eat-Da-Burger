6e6accf6093fe448dbaa28efc6b2b839ce90ac98
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-09-29 16:11:51 -0700

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 var mysql = require("mysql");
+var db = require("./db_config")
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    host: db.HOST,
+    user: db.USER,
+    password: db.PASSWORD,
+    database: db.DB
 });
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        throw err;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+//LOCAL CODE
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         throw err;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 module.exports = connection;
